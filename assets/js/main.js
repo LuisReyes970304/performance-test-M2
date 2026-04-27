8a54d00fa872994971f289a26d7a4969075abb88
LuisReyes970304 <luisrafaelreyescaro1@gmail.com>
2026-04-27 10:40:19 -0500

    js added

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,10 @@ const date = new Date()
 document.getElementById("year").innerText = date.getFullYear()
 
 // Write your Js code here 
+
+const menuToogle = document.querySelector(".menu-toggle");
+const navList = document.querySelector(".nav__list");
+
+menuToogle.addEventListener("click", () => {
+    navList.classList.toggle("show");
+});
